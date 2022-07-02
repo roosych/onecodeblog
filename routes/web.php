5c73dcf9d85d4276636e5ac6700dc8ac9c653757
kristimaza <kristinamazanova@bk.ru>
2022-07-02 15:23:32 +0400

    Deleted idea folder

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [BlogController::class, 'index'])->name('blog');
+
 Route::middleware('guest')->group(function(){
     Route::get('register', [RegisterController::class, 'index'])->name('register');
     Route::post('register', [RegisterController::class, 'store'])->name('register.store');
